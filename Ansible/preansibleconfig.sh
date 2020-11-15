319d90c28b0f7d6c37df6780faa871350aec7ab6
WarpWing <professornitro@gmail.com>
2020-11-15 19:47:24 +0000

    Update PAC

--- Ansible/preansibleconfig.sh
+++ Ansible/preansibleconfig.sh
@@ -1,2 +1,6 @@
 #!/bin/bash 
- 
+ # In order to deploy this, use the command below!
+ # Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/preansibleconfig.sh)"
+ clear 
+ echo Deploying Pre Flight Provisions!
+ sleep 2s
