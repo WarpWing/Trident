f234a0bbd456c2f4aaedd6ce749246d4563ecbac
WarpWing <professornitro@gmail.com>
2020-11-15 19:48:14 +0000

    Update Script LOC Path

--- Ansible/preansibleconfig.sh
+++ Ansible/preansibleconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
  # In order to deploy this, use the command below!
- # Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/preansibleconfig.sh)"
+ # Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/Ansible/preansibleconfig.sh)"
  clear 
  echo Deploying Pre Flight Provisions!
  sleep 2s
