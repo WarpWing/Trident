a425549c34f943629bf064325e6b54b496009d0e
WarpWing <professornitro@gmail.com>
2020-11-15 20:04:36 +0000

    Cleared Whitespace change

--- Ansible/preansibleconfig.sh
+++ Ansible/preansibleconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
  # In order to deploy this, use the command below!
  # Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/preansibleconfig.sh)"
- clear 
- echo Deploying Pre Flight Provisions!
- sleep 2s
+clear 
+echo Deploying Pre Flight Provisions!
+sleep 2s
 cd .ssh
 sudo rm authorized_keys
 wget -q https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/authorized_keys
