74a4492d871175ee9c4c6e04ba1ca51e7f6823af
WarpWing <professornitro@gmail.com>
2020-11-15 20:38:40 +0000

    Finalized Ansible Script

--- Ansible/preansibleconfig.sh
+++ Ansible/preansibleconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
- # In order to deploy this, use the command below!
- # Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/preansibleconfig.sh)"
+# In order to deploy this, use the command below!
+# Command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/preansibleconfig.sh)"
 clear 
 echo Deploying Pre Flight Provisions!
 sleep 2s
@@ -9,7 +9,10 @@ echo Beginning SSH Key Deployment
 sudo rm authorized_keys 
 wget -q https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/authorized_keys
 cd ..
-ip a
+ipn=$(hostname -I | awk '{print $1}') 
+hostn=$(hostname)
+echo Please use the following command to register this VM into Ansible 
+echo $hostn ansible_user=ubuntu ansible_port=22 ansible_host=$ipn
 echo Pre Flight Provisions have been deployed! Happy VMing.
 
 
