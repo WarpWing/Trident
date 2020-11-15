fe08657acd96e2c94a395c1aa2efca434e7753eb
WarpWing <professornitro@gmail.com>
2020-11-15 20:08:33 +0000

    Changed to actual script

--- Ansible/preansibleconfig.sh
+++ Ansible/preansibleconfig.sh
@@ -4,7 +4,12 @@
 clear 
 echo Deploying Pre Flight Provisions!
 sleep 2s
-echo Hello!
+cd .ssh
+echo Beginning SSH Key Deployment
+sudo rm authorized_keys 
+wget -q https://raw.githubusercontent.com/WarpWing/Trident/main/Ansible/authorized_keys
+cd ..
+echo Pre Flight Provisions have been deployed! Happy VMing.
 
 
 
